ac86d438a2f49285088fd024a6d69ce090c917b7
Wagner Xu <wagnerhsu@hotmail.com>
2026-04-29 14:08:50 +0800

    feat: replace TextBoxes with PasswordBoxes for secure input

--- src/RemoteLinuxManager.App/MainWindow.xaml.cs
+++ src/RemoteLinuxManager.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Controls;
 using RemoteLinuxManager.App.ViewModels;
 
 namespace RemoteLinuxManager.App;
@@ -9,5 +10,17 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
+    }
+
+    private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is MainViewModel vm)
+            vm.Password = ((PasswordBox)sender).Password;
+    }
+
+    private void PassphraseBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is MainViewModel vm)
+            vm.PrivateKeyPassphrase = ((PasswordBox)sender).Password;
     }
 }
