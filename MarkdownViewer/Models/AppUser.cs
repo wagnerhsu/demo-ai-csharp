2792e60f783633a1ed4c2a0d27d6e13a23f316c6
Wagner Xu <wagnerhsu@hotmail.com>
2026-04-30 13:17:33 +0800

    feat: move MarkdownViewer to demo-aspnetcore

--- MarkdownViewer/Models/AppUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace MarkdownViewer.Models;
-
-public class AppUser : IdentityUser
-{
-    public string DisplayName { get; set; } = string.Empty;
-}
--- MarkdownViewer/Pages/Account/AccessDenied.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace MarkdownViewer.Pages.Account;
-
-public class AccessDeniedModel : PageModel
-{
-    public void OnGet() { }
-}
