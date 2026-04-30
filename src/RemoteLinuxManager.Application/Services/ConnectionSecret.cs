0cdd2d05006754996c3ef27988c6cca42c10c6ae
Wagner Xu <wagnerhsu@hotmail.com>
2026-04-30 10:14:08 +0800

    feat: mote RemoteLinuxManager to demo-wpf

--- src/RemoteLinuxManager.Application/Services/ConnectionSecret.cs
+++
@@ -1,8 +0,0 @@
-namespace RemoteLinuxManager.Application.Services;
-
-public sealed record ConnectionSecret
-{
-    public string? Password { get; init; }
-
-    public string? PrivateKeyPassphrase { get; init; }
-}
--- src/RemoteLinuxManager.Domain/Models/AuthenticationType.cs
+++
@@ -1,7 +0,0 @@
-namespace RemoteLinuxManager.Domain.Models;
-
-public enum AuthenticationType
-{
-    Password,
-    PrivateKey
-}
